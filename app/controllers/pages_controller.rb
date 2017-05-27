831edd90b0688f12ea6068c9ac198de05ed1669a
Dumar Ojeda <dumarojeda@gmail.com>
2017-05-27 15:32:03 -0500

    Create seed for languages and create get them in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 	def show
-		render template: "pages/#{params[:page]}"
+    # Get all languages
+    @languages = Language.all
+    # Render
+    render template: "pages/#{params[:page]}"
 	end
 
 	def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Languages
+# Language.create(name: 'English', img_url: 'flag-united-state.svg')
+# Language.create(name: 'Spanish', img_url: 'flag-spain.svg')
+# Language.create(name: 'French', img_url: 'flag-france.svg')
+# Language.create(name: 'Japanese', img_url: 'flag-japan.svg')
+# Language.create(name: 'Italian', img_url: 'flag-italy.svg')
+# Language.create(name: 'German', img_url: 'flag-germany.svg')
