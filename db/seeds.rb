bd12c03e5d86283ea4e1a802fd86a7a8cea032f7
Javier Vargas <javier1615@gmail.com>
2017-05-27 18:49:32 -0500

    adding styles for home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Create Languages
-Language.create(name: 'English', img_url: 'flag-united-state.svg')
-Language.create(name: 'Spanish', img_url: 'flag-spain.svg')
-Language.create(name: 'French', img_url: 'flag-france.svg')
-Language.create(name: 'Japanese', img_url: 'flag-japan.svg')
-Language.create(name: 'Italian', img_url: 'flag-italy.svg')
-Language.create(name: 'German', img_url: 'flag-germany.svg')
+# # Create Languages
+# Language.create(name: 'English', img_url: 'flag-united-state.svg')
+# Language.create(name: 'Spanish', img_url: 'flag-spain.svg')
+# Language.create(name: 'French', img_url: 'flag-france.svg')
+# Language.create(name: 'Japanese', img_url: 'flag-japan.svg')
+# Language.create(name: 'Italian', img_url: 'flag-italy.svg')
+# Language.create(name: 'German', img_url: 'flag-germany.svg')
 
-# Create cities
-City.create(name:'Bogotá', img_url: 'city-bogota.jpg')
-City.create(name:'Buenos Aires', img_url: 'city-buenos-aires.jpg')
-City.create(name:'Mexico D.F', img_url: 'city-mexico-df.jpg')
-City.create(name:'São Paulo', img_url: 'city-sap-paulo.jpg')
-City.create(name:'Santiago de Chile', img_url: 'city-santiago-de-chile.jpg')
+# # Create cities
+# City.create(name:'Bogotá', img_url: 'city-bogota.jpg')
+# City.create(name:'Buenos Aires', img_url: 'city-buenos-aires.jpg')
+# City.create(name:'Mexico D.F', img_url: 'city-mexico-df.jpg')
+# City.create(name:'São Paulo', img_url: 'city-sap-paulo.jpg')
+# City.create(name:'Santiago de Chile', img_url: 'city-santiago-de-chile.jpg')
