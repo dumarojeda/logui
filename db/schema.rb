ee5dfcfc6dbb8001c19de4b81e359f5f43035115
melissa160 <melissa160@gmail.com>
2017-05-27 15:41:46 -0500

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527194546) do
+ActiveRecord::Schema.define(version: 20170527202754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20170527194546) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["guide_id"], name: "index_activities_on_guide_id", using: :btree
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "img_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "countries", force: :cascade do |t|
