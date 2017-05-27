2d193b513d850bc1a2643a0822b431d0a47eb166
Dumar Ojeda <dumarojeda@gmail.com>
2017-05-27 15:53:34 -0500

    Create seed for cities and create get them in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
 	def show
     # Get all languages
     @languages = Language.all
+    # Get all cities
+    @cities = City.all
     # Render
     render template: "pages/#{params[:page]}"
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,10 @@
 # Language.create(name: 'Japanese', img_url: 'flag-japan.svg')
 # Language.create(name: 'Italian', img_url: 'flag-italy.svg')
 # Language.create(name: 'German', img_url: 'flag-germany.svg')
+
+# Create cities
+# City.create(name:'Bogotá', img_url: 'city-bogota.jpg')
+# City.create(name:'Buenos Aires', img_url: 'city-buenos-aires.jpg')
+# City.create(name:'Mexico D.F', img_url: 'city-mexico-df.jpg')
+# City.create(name:'São Paulo', img_url: 'city-sap-paulo.jpg')
+# City.create(name:'Santiago de Chile', img_url: 'city-santiago-de-chile.jpg')
