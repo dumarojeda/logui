52a9b8d6b31d115c4b4e3b3bf8433fd5316c1a81
melissa160 <melissa160@gmail.com>
2017-05-27 19:27:34 -0500

    fixing seeds

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -2,7 +2,8 @@ class GuidesController < ApplicationController
 
   def index
     if params.has_key?(:city) && params.has_key?(:language)
-      @guides = Language.find(params[:language][:id]).guides
+      language = Language.find_by(name: params[:language]) 
+      @guides = Language.find(language.id).guides
       city = City.find_by(name: params[:city])
       @guides = @guides.where(city_id: city.id)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,11 +24,12 @@ Language.create(name: 'Italian', img_url: 'flag-italy.svg')
 Language.create(name: 'German', img_url: 'flag-germany.svg')
 
 # Create cities
-City.create(name:'Bogotá', img_url: 'city-bogota.jpg')
+City.create(name:'Bogota', img_url: 'city-bogota.jpg')
 City.create(name:'Buenos Aires', img_url: 'city-buenos-aires.jpg')
 City.create(name:'Mexico D.F', img_url: 'city-mexico-df.jpg')
-City.create(name:'São Paulo', img_url: 'city-sap-paulo.jpg')
+City.create(name:'Sao Paulo', img_url: 'city-sao-paulo.jpg')
 City.create(name:'Santiago de Chile', img_url: 'city-santiago-de-chile.jpg')
+City.create(name:'Lima', img_url: 'city-lima.jpg')
 
 # Create Guides 
 # pepito
