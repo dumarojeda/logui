1ba289fe7ee084c1edf39fa705428e872a593345
melissa160 <melissa160@gmail.com>
2017-05-27 20:16:29 -0500

    add include, hour, observations field to activity model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527225620) do
+ActiveRecord::Schema.define(version: 20170528011210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,20 @@ ActiveRecord::Schema.define(version: 20170527225620) do
     t.string   "img_url"
     t.integer  "price"
     t.integer  "guide_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "hour"
+    t.text     "include"
+    t.text     "observations"
     t.index ["guide_id"], name: "index_activities_on_guide_id", using: :btree
   end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "img_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "countries", force: :cascade do |t|
