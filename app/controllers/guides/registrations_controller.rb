81b99d679c82f9472f62784b73edc78e8e2daa72
Dumar Ojeda <dumarojeda@gmail.com>
2017-05-28 07:56:25 -0500

    Add permitted parameters

--- app/controllers/guides/registrations_controller.rb
+++ app/controllers/guides/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Guides::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -57,4 +58,10 @@ class Guides::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city_id])
+      devise_parameter_sanitizer.permit(:user_update, keys: [:name, :city_id])
+    end
 end
